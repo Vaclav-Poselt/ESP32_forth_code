c1f636678d28e98fd776eb817368f0e231027881
vposelt <vposelt@volny.cz>
2024-12-29 22:58:49 +0100

    error repair
    json userwords.h  error repair

--- ESP32FORTH and WiFi_JSON/JSON_time_request/userwords.h
+++ ESP32FORTH and WiFi_JSON/JSON_time_request/userwords.h
@@ -12,7 +12,7 @@ http.getString ( payloadbufer size--- payloadbuffer ) reads respond max size byt
 http.end ( --- ) ends connection with url    
 JSON.deserialize ( z-payload errorbuf---z-errorbuf ) create deserialized doc
     from z-payload, in z-errorbuf is "OK" or error text 
-JSON.getString ( stringbuf z-key---z-stringbuf ) retrive string key value
+JSON.getString ( stringbuf stringbuffersize z-key---z-stringbuf ) retrive string key value
     to stringbuf
 JSON.getNum ( z-key--- n) retrive numerical key value, also for bool as 1/0
 */
@@ -21,11 +21,11 @@ JSON.getNum ( z-key--- n) retrive numerical key value, also for bool as 1/0
 X("HTTP.begin", httpbegin, n0=http.begin(c0); ) \
 X("HTTP.get", httpget, PUSH http.GET(); ) \
 X("HTTP.getString", httpgetstring, const char* zpay=http.getString().c_str(); \
-    strlcpy(c1, zpay, strlen(c1)); DROP;) \
+    strlcpy(c1, zpay, n0); DROP;) \
 X("HTTP.end", httpend, http.end(); ) \
 X("JSON.deserialize", jsondeserialize, DeserializationError error = deserializeJson(doc, c1); \ 
     strcpy(c0, error.c_str()) ; NIP ) \
 X("JSON.getString", jsongetstring, const char* retrievedValue=doc[c0].as<const char*>(); \
-     strlcpy(c1, retrievedValue, strlen(c1)); DROP; ) \
+     strlcpy(c2, retrievedValue, n1); DROPn(2); ) \
 X("JSON.getNum", jsongetnum, n0=doc[c0]; )    
 
